0cf78c7d81e073447a7d76eeb57c939057b2e239
zustinanasco0 <zustinanasco0@gmail.com>
2026-05-08 01:39:04 -0500

    deleted the (succesfully connected database)

--- connectiondb.php
+++ connectiondb.php
@@ -16,5 +16,5 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "<h1>Successfully connected to the database!</h1>";
+
 ?>
