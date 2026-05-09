2c54c0f6baa853b1dd825bf79747d61cf1d77ffa
zustinanasco0 <zustinanasco0@gmail.com>
2026-05-09 00:42:52 -0500

    Added A JOIN shems minecraft time

--- member_register.php
+++ member_register.php
@@ -1,3 +1,27 @@
 <?php
 include 'connectiondb.php';
 $message = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $fname = mysqli_real_escape_string($conn, $_POST['fname']);
+    $lname = mysqli_real_escape_string($conn, $_POST['lname']);
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT); // Secure hashing
+    $plan_id = $_POST['plan_id'];
+
+    // 1. Insert into 'members' table first
+    $sql_member = "INSERT INTO members (fname, lname, plan_id) VALUES ('$fname', '$lname', '$plan_id')";
+    
+    if ($conn->query($sql_member) === TRUE) {
+        $new_member_id = $conn->insert_id; // Get the ID created for this member
+
+        // 2. Insert into 'member_accounts' table using that ID
+        $sql_account = "INSERT INTO member_accounts (member_id, username, password) VALUES ('$new_member_id', '$username', '$password')";
+        
+        if ($conn->query($sql_account) === TRUE) {
+            $message = "success";
+        }
+    } else {
+        $message = "error";
+    }
+}
